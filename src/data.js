3496df883a730527aff4f1c168d6c8c061412a19
Andy Caseres <andycaseres2@gmail.com>
2023-08-10 19:49:38 -0500

    portfolio  Astro

---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const TechSkills = [
+  {
+    icon: '/src/assets/images/Html.webp',
+  },
+  {
+    icon: '/src/assets/images/Css.webp',
+  },
+  {
+    icon: '/src/assets/images/Javascript.webp',
+  },
+  {
+    icon: '/src/assets/images/Typescript.webp',
+  },
+  {
+    icon: '/src/assets/images/react.webp',
+  },
+  {
+    icon: '/src/assets/images/next.webp',
+  },
+  {
+    icon: '/src/assets/images/astro.webp',
+  },
+  {
+    icon: '/src/assets/images/Gatsby.webp',
+  },
+  {
+    icon: '/src/assets/images/GraphQL.webp',
+  },
+  {
+    icon: '/src/assets/images/Redux.webp',
+  },
+  {
+    icon: '/src/assets/images/Bootstrap.webp',
+  },
+  {
+    icon: '/src/assets/images/Vite.webp',
+  },
+  {
+    icon: '/src/assets/images/BitBucket.webp',
+  },
+  {
+    icon: '/src/assets/images/Jira.webp',
+  },
+  {
+    icon: '/src/assets/images/Git.webp',
+  },
+  {
+    icon: '/src/assets/images/Github.webp',
+  },
+  {
+    icon: '/src/assets/images/Sass.webp',
+  },
+  {
+    icon: '/src/assets/images/Tailwind.webp',
+  },
+  {
+    icon: '/src/assets/images/MaterialUI.webp',
+  },
+  {
+    icon: '/src/assets/images/Storybook.webp',
+  },
+  {
+    icon: '/src/assets/images/Strapi.webp',
+  },
+];
---
+++ src/navigation.js
@@ -0,0 +1,25 @@
+export const headerData = {
+  links: [
+    {
+      text: 'About',
+      href: '#about',
+    },
+    {
+      text: 'Projects',
+      href: '#projects',
+    },
+    {
+      text: 'Experience',
+      href: '#experience',
+    },
+  ],
+  actions: [{ type: 'button', text: 'Download', href: 'https://github.com/onwidget/AndyCaseres' }],
+};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/andy-caseres/' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/andycaseres2' },
+    { ariaLabel: 'Twitter', icon: 'tabler:brand-twitter', href: 'https://twitter.com/AndyCaseresDev' },
+  ],
+};
