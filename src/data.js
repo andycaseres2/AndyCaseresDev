c08d75fa60a8baa18a42bb28e85f5c390be63668
Andy Caseres <andycaseres2@gmail.com>
2023-08-13 14:50:47 -0500

    useing cdn for image

--- src/data.js
+++ src/data.js
@@ -1,65 +1,198 @@
 export const TechSkills = [
   {
-    icon: '../src/assets/images/Javascript.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
   },
   {
-    icon: '/src/assets/images/Css.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Css.webp?updatedAt=1691953648743',
   },
   {
-    icon: '/src/assets/images/Javascript.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
   },
   {
-    icon: '/src/assets/images/Typescript.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Typescript.webp?updatedAt=1691953657810',
   },
   {
-    icon: '/src/assets/images/react.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/React.webp?updatedAt=1691953654127',
   },
   {
-    icon: '/src/assets/images/next.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Next.webp?updatedAt=1691953653290',
   },
   {
-    icon: '/src/assets/images/astro.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/astro.webp?updatedAt=1691953647273',
   },
   {
-    icon: '/src/assets/images/Gatsby.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Gatsby.webp?updatedAt=1691953649818',
   },
   {
-    icon: '/src/assets/images/GraphQL.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/GraphQL.webp?updatedAt=1691953650063',
   },
   {
-    icon: '/src/assets/images/Redux.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Redux.webp?updatedAt=1691953654517',
   },
   {
-    icon: '/src/assets/images/Bootstrap.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Bootstrap.webp?updatedAt=1691953648094',
   },
   {
-    icon: '/src/assets/images/Vite.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Vite.webp?updatedAt=1691953657957',
   },
   {
-    icon: '/src/assets/images/BitBucket.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/BitBucket.webp?updatedAt=1691953646785',
   },
   {
-    icon: '/src/assets/images/Jira.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Jira.webp?updatedAt=1691953652244',
   },
   {
-    icon: '/src/assets/images/Git.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Git.webp?updatedAt=1691953649965',
   },
   {
-    icon: '/src/assets/images/Github.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Github.webp?updatedAt=1691953650177',
   },
   {
-    icon: '/src/assets/images/Sass.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Sass.webp?updatedAt=1691953654426',
   },
   {
-    icon: '/src/assets/images/Tailwind.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Tailwind.webp?updatedAt=1691953657236',
   },
   {
-    icon: '/src/assets/images/MaterialUI.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/MaterialUI.webp?updatedAt=1691953652865',
   },
   {
-    icon: '/src/assets/images/Storybook.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Storybook.webp?updatedAt=1691953654587',
   },
   {
-    icon: '/src/assets/images/Strapi.webp',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Strapi.webp?updatedAt=1691953656830',
+  },
+];
+
+export const AnimeAc = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'ReactJs',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/React.webp?updatedAt=1691953654127',
+  },
+  {
+    title: 'TailwindCSS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Tailwind.webp?updatedAt=1691953657236',
+  },
+];
+
+export const Bellpi = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'GatsbyJS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Gatsby.webp?updatedAt=1691953649818',
+  },
+  {
+    title: 'NextJs',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Next.webp?updatedAt=1691953653290',
+  },
+  {
+    title: 'GraphQL',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/GraphQL.webp?updatedAt=1691953650063',
+  },
+  {
+    title: 'Strapi',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Strapi.webp?updatedAt=1691953656830',
+  },
+];
+
+export const Datarte = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'React',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/React.webp?updatedAt=1691953654127',
+  },
+  {
+    title: 'Redux',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Redux.webp?updatedAt=1691953654517',
+  },
+  {
+    title: 'TailwindCSS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Tailwind.webp?updatedAt=1691953657236',
+  },
+];
+
+export const Acflix = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'ReactJs',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/React.webp?updatedAt=1691953654127',
+  },
+  {
+    title: 'TailwindCSS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Tailwind.webp?updatedAt=1691953657236',
+  },
+  {
+    title: 'Firebase',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/firebase.webp?updatedAt=1691953649798',
+  },
+];
+
+export const VideoAc = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'ReactJs',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/React.webp?updatedAt=1691953654127',
+  },
+  {
+    title: 'CSS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Css.webp?updatedAt=1691953648743',
+  },
+];
+
+export const ConversionAc = [
+  {
+    title: 'JavaScript',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Javascript.webp?updatedAt=1691953652824',
+  },
+  {
+    title: 'NextJs',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Next.webp?updatedAt=1691953653290',
+  },
+  {
+    title: 'TailwindCSS',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/Tailwind.webp?updatedAt=1691953657236',
+  },
+];
+
+export const Experience = [
+  {
+    title: 'Frontend Developer Junior',
+    description:
+      'Contribute to the creation of new components, optimize the architecture and improve the user experience in applications.',
+    description2:
+      'Implement new features and functionality, collaborating closely with the design team and other developers to ensure a high-quality final product.',
+    description3: 'Perform code refactorings to improve application efficiency, scalability, and maintainability.',
+    description4:
+      'Detect and fix bugs and technical issues proactively, working collaboratively with the team to ensure a bug-free product.',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/bellpi.svg?updatedAt=1691953648368',
+    techSkill: 'Bitbucket · GatsbyJS · GraphQL · Next.js · React.js · JavaScript · Jira · Git',
+    time: 'December 2022 - Currently',
+  },
+  {
+    title: 'Frontend Developer Junior',
+    description:
+      'Collaborate in the creation of attractive and functional user interfaces, making sure that they comply with design and usability standards.',
+    description2: 'Develop and create new components to improve the architecture and user experience of applications.',
+    description3:
+      'Proactively identify and fix errors and technical issues (bugs), working closely with the team to ensure quality delivery.',
+    icon: 'https://ik.imagekit.io/andycaseres/portfolio/datarte.svg?updatedAt=1691953648386',
+    techSkill: 'ReactJs · ReduxJs · TalwindCss · JavaScript · Storybook · Git',
+    time: 'May 2022 - October 2022',
   },
 ];
